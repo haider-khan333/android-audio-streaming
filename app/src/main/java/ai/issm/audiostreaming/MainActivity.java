a1b65542e783c22be24df6365f342cf29ca0f991
haider-khan333 <haider.khan4444@hotmail.com>
2023-09-18 14:28:51 +0500

    first commit

---
+++ app/src/main/java/ai/issm/audiostreaming/MainActivity.java
@@ -0,0 +1,21 @@
+package ai.issm.audiostreaming;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView);
+        textView.setOnClickListener(e-> System.out.println("Hello World"));
+
+    }
+}
